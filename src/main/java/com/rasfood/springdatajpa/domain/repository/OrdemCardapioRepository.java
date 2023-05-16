46d43b174481f7153dcd4f11627f6f42db8ff818
Nicolas <nicolasjandre@live.com>
2023-05-16 11:13:50 -0300

    :fire: removing unused OrdemCardapioRepository

--- src/main/java/com/rasfood/springdatajpa/domain/repository/OrdemCardapioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rasfood.springdatajpa.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rasfood.springdatajpa.domain.model.OrdemCardapio;
-
-public interface OrdemCardapioRepository extends JpaRepository<OrdemCardapio, Long> {
-}
